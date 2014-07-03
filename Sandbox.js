ab4ee78f6ad694e5f3cd74250e1f123910b7c6ee
Add Chan <addison95132@gmail.com>
2014-07-03 11:39:16 -0700

    Update Sandbox.js

--- Sandbox.js
+++ Sandbox.js
@@ -1,4 +1,15 @@
-//var hovr = 
+$(document).ready(
+  var hovr = function(thing, clas){//clas is the class because class is defined in js already.
+  $(thing).hover(function(){
+    $(thing).addClass(clas)
+  }, function(){
+    $(thing).removeClass(clas)
+  })
+  }//end for var function
+  hovr("div", "blue")
+  )
+  
+/*BKUP
 $(document).ready(
   function(){//clas is the class because class is defined in js already.
   $('div').hover(function(){
@@ -6,7 +17,6 @@ $(document).ready(
   }, function(){
     $('div').removeClass('blue')
   })
-  }
-  //hovr("div", "blue")
-  )
+  })
   
+*/
